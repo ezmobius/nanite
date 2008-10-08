411cecd5d62891939f4f86ef8a5f69925dd69018
bmizerany <blake.mizerany@gmail.com>
2008-10-08 12:46:19 -0700

    Bug Fixes:
    * Remove uneeded require.
    * Fixed console:  No longer blows up for simple errors.
    * Removed trailing white spaces.

--- lib/nanite.rb
+++ lib/nanite.rb
@@ -10,27 +10,27 @@ require 'nanite/streaming'
 require 'extlib'
 
 module Nanite
-  
+
   VERSION = '0.1' unless defined?(Nanite::VERSION)
-  
+
   class << self
     attr_accessor :identity, :status_proc, :results, :root, :vhost, :file_root, :files, :host
-    
+
     attr_accessor :default_services, :last_ping, :ping_time
-    
+
     include FileStreaming
-            
+
     def send_ping
       ping = Nanite::Ping.new(Nanite.identity, Nanite.status_proc.call)
       Nanite.amq.topic('heartbeat').publish(Marshal.dump(ping), :key => 'nanite.pings')
     end
-    
+
     def advertise_services
       p "advertise_services",Nanite::Dispatcher.all_services
       reg = Nanite::Register.new(Nanite.identity, Nanite::Dispatcher.all_services, Nanite.status_proc.call)
       Nanite.amq.topic('registration').publish(Marshal.dump(reg), :key => 'nanite.register')
     end
-    
+
     def start_console
       puts "starting console"
       require 'readline'
@@ -38,19 +38,23 @@ module Nanite
         while l = Readline.readline('>> ')
           unless l.nil? or l.strip.empty?
             Readline::HISTORY.push(l)
-            eval l
+            begin
+              eval l
+            rescue => e
+              puts "#{e.class.name}: #{e.message}\n  #{e.backtrace.join("\n  ")}"
+            end
           end
         end
       }
     end
-    
+
     def load_actors
       Dir["#{Nanite.root}/actors/*.rb"].each do |actor|
         puts "loading actor: #{actor}"
         require actor
-      end  
+      end
     end
-    
+
     def start(opts={})
       config = YAML::load(IO.read(File.expand_path(File.join(opts[:root], 'config.yml')))) rescue {}
       opts = config.merge(opts)
@@ -66,51 +70,51 @@ module Nanite
                  :vhost => Nanite.vhost,
                  :host  => Nanite.host,
                  :port  => (opts[:port] || ::AMQP::PORT).to_i
-      
+
       load_actors
       advertise_services
-                              
+
       EM.add_periodic_timer(15) do
         send_ping
       end
-      
+
       Nanite.amq.queue(Nanite.identity, :exclusive => true).subscribe{ |msg|
         Nanite::Dispatcher.handle(Marshal.load(msg))
-      }      
+      }
       start_console if opts[:console]
-    end  
-    
+    end
+
     def reducer
       @reducer ||= Nanite::Reducer.new
     end
-    
+
     def status_proc
       @status_proc ||= lambda{ parse_uptime(`uptime`) rescue "no status"}
     end
-    
+
     def parse_uptime(up)
       if up =~ /load averages?: (.*)/
         a,b,c = $1.split(/\s+|,\s+/)
         (a.to_f + b.to_f + c.to_f) / 3
       end
     end
-      
+
     def amq
       Thread.current[:mq] ||= MQ.new
     end
-    
+
     def pending
       @pending ||= {}
     end
-    
+
     def callbacks
       @callbacks ||= {}
     end
-    
+
     def results
       @results ||= {}
     end
-    
+
     def gen_token
       values = [
         rand(0x0010000),
@@ -123,5 +127,5 @@ module Nanite
       ]
       "%04x%04x%04x%04x%04x%06x%06x" % values
     end
-  end  
-end  
+  end
+end
