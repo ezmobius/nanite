7b735e79d4f1237d0ce87dfacea6c4e733858ca9
Glenn Rempe <glenn@rempe.us>
2009-01-01 14:05:14 -0800

    cli.rb can be run as an executable.  Calls new simpleagent example code.

--- examples/cli.rb
+++ examples/cli.rb
@@ -1,18 +1,19 @@
+#!/usr/bin/env ruby
+
 require 'rubygems'
 require 'nanite'
 require 'nanite/mapper'
- 
+
 Nanite.identity = Nanite.gensym
- 
- 
+
 EM.run {
-  AMQP.start :host => 'localhost', :user => 'mapper', :pass => 'testing',
-             :vhost => '/nanite'
+  AMQP.start(:host => 'localhost', :user => 'mapper', :pass => 'testing', :vhost => '/nanite')
   Nanite.mapper = Nanite::Mapper.new(15)
   EM.add_timer(16) do
-    Nanite.request("/mock/list", '', :target => 'barney') do |res|
+    Nanite.request("/simple/hello", '') do |res|
       p res
       EM.stop_event_loop
     end
-  end  
+  end
 }
+
