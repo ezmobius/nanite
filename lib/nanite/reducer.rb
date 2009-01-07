2cf002e247b2a8968a793b47b0df25471b0794fd
Michael S. Klishin <michael@novemberain.com>
2009-01-07 15:12:06 +0300

    Documentation for Nanite::Reducer and Nanite::Answer.

--- lib/nanite/reducer.rb
+++ lib/nanite/reducer.rb
@@ -1,5 +1,9 @@
 module Nanite
-
+  # Represents reply to a work request from a particular node
+  # in the nanite cluster.
+  #
+  # token is request unique identifier
+  # agent is a node reply was received from
   class Answer
     attr_accessor :token, :results, :workers, :agent
     def initialize(agent,token)
@@ -9,9 +13,13 @@ module Nanite
     end
 
     def handle_result(res)
-      
+      # pick a result from the node and remove sender
+      # from the list of workers we are yet to receive reply
+      # from
       @results[res.from] = res.results
       @workers.delete(res.from)
+      # when all workers successfully replied, fire a callback
+      # and delete request token from timeouts list
       if @workers.empty?
         cback = @agent.callbacks.delete(@token)
         cback.call(@results) if cback
@@ -34,6 +42,7 @@ module Nanite
     end
 
     def handle_result(res)
+      # response here is an answer instance
       if response = @responses[res.token]
         if response.handle_result(res)
           @responses.delete(res.token)
