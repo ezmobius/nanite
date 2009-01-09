bcdf8bfab054e9f8404d465cb862a9fa26d95191
Glenn Rempe <glenn@rempe.us>
2009-01-09 13:39:00 -0800

    Consolidate previous myagent example into simpleagent

--- examples/simpleagent/actors/simple.rb
+++ examples/simpleagent/actors/simple.rb
@@ -2,10 +2,18 @@
 
 class Simple < Nanite::Actor
 
-  expose :echo
+  expose :echo, :time, :gems
 
   def echo(payload)
     "Nanite said #{payload.empty? ? "nothing at all" : payload} @ #{Time.now.to_s}"
+  end
+
+  def time(payload)
+    Time.now
+  end
+
+  def gems(filter)
+    ::Gem.source_index.refresh!.search(filter).flatten.collect {|gemspec| "#{gemspec.name} #{gemspec.version}"}  
   end
 
 end
