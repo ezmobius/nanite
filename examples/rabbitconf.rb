9da8c6a2561be68fed7c96ec0103d13b832bdc10
Ezra Zygmuntowicz <ez@engineyard.com>
2009-01-31 23:24:54 -0800

    adding permissions stuff to rabbitconf.rb, the permissions only works on the rabbit broker from hg HEAD. you can ignore the failed commands if you are on an older broker

--- examples/rabbitconf.rb
+++ examples/rabbitconf.rb
@@ -7,7 +7,11 @@ puts `rabbitmqctl add_vhost /nanite`
   puts `rabbitmqctl map_user_vhost #{agent} /nanite`
 end
 
+puts `scripts/rabbitmqctl set_permissions -p /nanite mapper '.*' ".*"`
+puts `scripts/rabbitmqctl set_permissions -p /nanite nanite '^nanite.*' ".*"`
+
 puts `rabbitmqctl  list_vhosts`
 puts `rabbitmqctl  list_users`
+puts `rabbitmqctl  list_permissions`
 puts `rabbitmqctl list_vhost_users /nanite`
 
