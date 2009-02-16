f1499cbf60eca003d772c2b262586eae03995321
Ian Leitch <ian.leitch@systino.net>
2009-02-16 11:49:47 -0500

    Moved Job and JobWarden into job.rb
    Signed-off-by: Ian Leitch <ian.leitch@systino.net>

---
+++ lib/nanite/job.rb
@@ -0,0 +1,50 @@
+module Nanite
+  class JobWarden
+    attr_reader :serializer, :jobs, :log
+
+    def initialize(serializer, log)
+      @serializer = serializer
+      @log = log
+      @jobs = {}
+    end
+
+    def new_job(request, targets, blk = nil)
+      job = Job.new(request, targets, blk)
+      jobs[job.token] = job
+      job
+    end
+
+    def process(msg)
+      msg = serializer.load(msg)
+      log.debug("processing message: #{msg.inspect}")
+      if job = jobs[msg.token]
+        job.process(msg)
+        if job.completed?
+          jobs.delete(job.token)
+          job.completed.call(job.results) if job.completed
+        end
+      end
+    end
+  end
+
+  class Job
+    attr_reader :results, :request, :token, :targets, :completed
+
+    def initialize(request, targets, blk)
+      @request = request
+      @targets = targets
+      @token = @request.token
+      @results = {}
+      @completed = blk
+    end
+
+    def process(msg)
+      results[msg.from] = msg.results
+      targets.delete(msg.from)
+    end
+
+    def completed?
+      targets.empty?
+    end
+  end
+end
