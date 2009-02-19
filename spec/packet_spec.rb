363cb20eec72bed3b9647ce99d1a1f9b76cfbf10
ck <ckatgithub@gmail.com>
2009-02-19 16:08:52 -0500

    Changes around the Packet (base class).
    Made following changes related to Nanite::Packet:
    
    - made it 'abstract', since it does not make any sense to instantiate it
    - added specs for Packet class 
    - Formated code (removed spaces, etc.) by running it through Ruby Beautifier by Paul Lutus

--- spec/packet_spec.rb
+++ spec/packet_spec.rb
@@ -2,6 +2,52 @@ require File.dirname(__FILE__) + '/spec_helper'
 require 'nanite/packets'
 require 'json'
 
+describe "Packet: Base class" do
+  before(:all) do
+    class TestPacket < Nanite::Packet
+      @@cls_attr = "ignore"
+      def initialize(attr1)
+        @attr1 = attr1
+      end
+    end
+  end
+
+  it "should be an abstract class" do
+    lambda { Nanite::Packet.new }.should raise_error(NotImplementedError, "Nanite::Packet is an abstract class.")
+  end
+
+  it "should know how to dump itself to JSON" do
+    packet = TestPacket.new(1)
+    packet.should respond_to(:to_json)
+  end
+
+  it "should dump the class name in 'json_class' JSON key" do
+    packet = TestPacket.new(42)
+    packet.to_json().should =~ /\"json_class\":\"TestPacket\"/
+  end
+
+  it "should dump instance variables in 'data' JSON key" do
+    packet = TestPacket.new(188)
+    packet.to_json().should =~ /\"data\":\{\"attr1\":188\}/
+  end
+
+  it "should not dump class variables" do
+    packet = TestPacket.new(382)
+    packet.to_json().should_not =~ /cls_attr/
+  end
+
+  it "should store instance variables in 'data' JSON key as JSON object" do
+    packet = TestPacket.new(382)
+    packet.to_json().should =~ /\"data\":\{[\w:"]+\}/
+  end
+
+  it "should remove '@' from instance variables" do
+    packet = TestPacket.new(2)
+    packet.to_json().should_not =~ /@attr1/
+    packet.to_json().should =~ /attr1/
+  end
+end
+
 describe "Packet: FileStart" do
   it "should dump/load as JSON objects" do
     packet = Nanite::FileStart.new('foo.txt', 'somewhere/foo.txt', '0xdeadbeef')
@@ -10,7 +56,7 @@ describe "Packet: FileStart" do
     packet.dest.should == packet2.dest
     packet.token.should == packet2.token
   end
-  
+
   it "should dump/load as Marshalled ruby objects" do
     packet = Nanite::FileStart.new('foo.txt', 'somewhere/foo.txt', '0xdeadbeef')
     packet2 = Marshal.load(Marshal.dump(packet))
@@ -27,7 +73,7 @@ describe "Packet: FileEnd" do
     packet.meta.should == packet2.meta
     packet.token.should == packet2.token
   end
-  
+
   it "should dump/load as Marshalled ruby objects" do
     packet = Nanite::FileEnd.new('0xdeadbeef', 'metadata')
     packet2 = Marshal.load(Marshal.dump(packet))
@@ -43,7 +89,7 @@ describe "Packet: FileChunk" do
     packet.chunk.should == packet2.chunk
     packet.token.should == packet2.token
   end
-  
+
   it "should dump/load as Marshalled ruby objects" do
     packet = Nanite::FileChunk.new('chunk','0xdeadbeef')
     packet2 = Marshal.load(Marshal.dump(packet))
@@ -62,7 +108,7 @@ describe "Packet: Request" do
     packet.token.should == packet2.token
     packet.reply_to.should == packet2.reply_to
   end
-  
+
   it "should dump/load as Marshalled ruby objects" do
     packet = Nanite::Request.new('/some/foo', 'payload', :from => 'from', :token => '0xdeadbeef', :reply_to => 'reply_to')
     packet2 = Marshal.load(Marshal.dump(packet))
@@ -70,7 +116,7 @@ describe "Packet: Request" do
     packet.payload.should == packet2.payload
     packet.from.should == packet2.from
     packet.token.should == packet2.token
-    packet.reply_to.should == packet2.reply_to  
+    packet.reply_to.should == packet2.reply_to
   end
 end
 
@@ -84,14 +130,14 @@ describe "Packet: Result" do
     packet.results.should == packet2.results
     packet.from.should == packet2.from
   end
-  
+
   it "should dump/load as Marshalled ruby objects" do
     packet = Nanite::Result.new('0xdeadbeef', 'to', 'results', 'from')
     packet2 = Marshal.load(Marshal.dump(packet))
     packet.token.should == packet2.token
     packet.to.should == packet2.to
     packet.results.should == packet2.results
-    packet.from.should == packet2.from  
+    packet.from.should == packet2.from
   end
 end
 
@@ -103,7 +149,7 @@ describe "Packet: Register" do
     packet.services.should == packet2.services
     packet.status.should == packet2.status
   end
-  
+
   it "should dump/load as Marshalled ruby objects" do
     packet = Nanite::Register.new('0xdeadbeef', ['/foo/bar', '/nik/qux'], 0.8)
     packet2 = Marshal.load(Marshal.dump(packet))
@@ -120,7 +166,7 @@ describe "Packet: Ping" do
     packet.identity.should == packet2.identity
     packet.status.should == packet2.status
   end
-  
+
   it "should dump/load as Marshalled ruby objects" do
     packet = Nanite::Ping.new('0xdeadbeef', 0.8)
     packet2 = Marshal.load(Marshal.dump(packet))
