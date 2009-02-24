9a9642f89695a35f3c185cb8b5742aea6f1fea48
ck <ckatgithub@gmail.com>
2009-02-23 20:21:58 -0500

    Added specs for additional String (util) methods.
    Ran code formatter.

--- lib/nanite/util.rb
+++ lib/nanite/util.rb
@@ -12,9 +12,9 @@ class String
   def snake_case
     return self.downcase if self =~ /^[A-Z]+$/
     self.gsub(/([A-Z]+)(?=[A-Z][a-z]?)|\B[A-Z]/, '_\&') =~ /_*(.*)/
-      return $+.downcase
+    return $+.downcase
   end
-  
+
   ##
   # Convert a constant name to a path, assuming a conventional structure.
   #
