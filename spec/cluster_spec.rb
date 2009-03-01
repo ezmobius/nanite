9f330d4627eef7f6039e2719b0985818b25aadf4
ck <ckatgithub@gmail.com>
2009-03-01 10:00:08 -0500

    Added specs for Nanite::Cluster.

---
+++ spec/cluster_spec.rb
@@ -0,0 +1,77 @@
+require File.dirname(__FILE__) + '/spec_helper'
+require 'nanite'
+
+describe Nanite::Cluster do
+
+  describe "Intialization" do
+
+    before(:each) do
+      @fanout = mock("fanout")
+      @binding = mock("binding", :subscribe => true)
+      @queue = mock("queue", :bind => @binding)
+      @amq = mock("AMQueue", :queue => @queue, :fanout => @fanout)
+      @log = mock("Log")
+      @serializer = mock("Serializer")
+      @reaper = mock("Reaper")
+      Nanite::Reaper.stub!(:new).and_return(@reaper)
+    end
+
+    describe "of Heartbeat (Queue)" do
+
+      it "should setup the heartbeat (queue) for id" do
+        @amq.should_receive(:queue).with("heartbeat-the_identity", anything()).and_return(@queue)
+        cluster = Nanite::Cluster.new(@amq, 10, "the_identity", @log, @serializer)
+      end
+
+      it "should make the heartbeat (queue) exclusive" do
+        @amq.should_receive(:queue).with("heartbeat-the_identity", { :exclusive => true }).and_return(@queue)
+        cluster = Nanite::Cluster.new(@amq, 10, "the_identity", @log, @serializer)
+      end
+
+      it "should bind the heartbeat (queue) to 'heartbeat' fanout" do
+        @amq.should_receive(:fanout).with("heartbeat", { :durable => true }).and_return(@fanout)
+        @queue.should_receive(:bind).with(@fanout).and_return(@binding)
+        cluster = Nanite::Cluster.new(@amq, 10, "the_identity", @log, @serializer)
+      end
+
+    end # of Heartbeat (Queue)
+
+
+    describe "of Registration (Queue)" do
+
+      it "should setup the registration (queue) for id" do
+        @amq.should_receive(:queue).with("registration-the_identity", anything()).and_return(@queue)
+        cluster = Nanite::Cluster.new(@amq, 10, "the_identity", @log, @serializer)
+      end
+
+      it "should make the registration (queue) exclusive" do
+        @amq.should_receive(:queue).with("registration-the_identity", { :exclusive => true }).and_return(@queue)
+        cluster = Nanite::Cluster.new(@amq, 10, "the_identity", @log, @serializer)
+      end
+
+      it "should bind the registration (queue) to 'registration' fanout" do
+        @amq.should_receive(:fanout).with("registration", { :durable => true }).and_return(@fanout)
+        @queue.should_receive(:bind).with(@fanout).and_return(@binding)
+        cluster = Nanite::Cluster.new(@amq, 10, "the_identity", @log, @serializer)
+      end
+
+    end # of Registration (Queue)
+
+
+    describe "Reaper" do
+      
+      it "should be created" do
+        Nanite::Reaper.should_receive(:new).with(anything()).and_return(@reaper)
+        cluster = Nanite::Cluster.new(@amq, 32, "the_identity", @log, @serializer)
+      end
+
+      it "should use the agent timeout" do
+        Nanite::Reaper.should_receive(:new).with(443).and_return(@reaper)
+        cluster = Nanite::Cluster.new(@amq, 443, "the_identity", @log, @serializer)
+      end
+      
+    end # Reaper
+
+  end # Intialization
+
+end # Nanite::Cluster
