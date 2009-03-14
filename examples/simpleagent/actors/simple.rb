7c3832ebde1d6a5a358088c6c687d1ba06741806
Lee Nussbaum <wln@scrunch.org>
2009-03-13 21:19:30 -0400

    Initial partial implementation of IntermediateState facilities; does not yet provide callbacks to signal arrival of intermediate state notices.
    * Agent can yield while running: one argument is delivered as intermediate state using a default key; yielding with two arguments delivers the value (second arg) using a provided key (first arg).
    * Job completion callbacks now deliver the job object, in addition to just the results, if the provided result block gives a second argument.
    * Admin ui now shows intermediate state at end of operation, if any was delivered.
    * Added a sample use to simpleagent.

--- examples/simpleagent/actors/simple.rb
+++ examples/simpleagent/actors/simple.rb
@@ -2,7 +2,7 @@
 
 class Simple
   include Nanite::Actor
-  expose :echo, :time, :gems
+  expose :echo, :time, :gems, :yielding
 
   def echo(payload)
     "Nanite said #{payload.empty? ? "nothing at all" : payload} @ #{Time.now.to_s}"
@@ -10,6 +10,19 @@ class Simple
 
   def time(payload)
     Time.now
+  end
+
+  def yielding(payload)
+    3.times do
+      yield :random, "%06x%06x" % [ rand(0x1000000), rand(0x1000000) ]
+    end
+    [1,2,3].each do |num|
+      yield :testkey, num
+    end
+    ["a","b","c"].each do |val|
+      yield val
+    end
+    "Nanite said #{payload.empty? ? "nothing at all" : payload} @ #{Time.now.to_s}"
   end
 
   def gems(filter)
