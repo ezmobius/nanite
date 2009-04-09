d4fd749db1fea7c770c6caec89c542d75125f9b8
Fabien Jakimowicz <fabien@jakimowicz.com>
2009-04-09 20:42:30 +0200

    actors are loaded AFTER daemonizing, we can't chdir on daemonzing process. Sorry for the last modification

--- lib/nanite/daemonize.rb
+++ lib/nanite/daemonize.rb
@@ -4,7 +4,6 @@ module Nanite
       exit if fork
       Process.setsid
       exit if fork
-      Dir.chdir "/"
       File.umask 0000
       STDIN.reopen "/dev/null"
       STDOUT.reopen "/dev/null", "a"
