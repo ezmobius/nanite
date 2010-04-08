1c05ffb4d30a7b37f427f637d202de8873e380e7
Mathias Meyer <meyer@paperplanes.de>
2010-04-08 15:09:50 +0200

    Bump test release version.

--- lib/nanite.rb
+++ lib/nanite.rb
@@ -41,7 +41,7 @@ require 'nanite/security/static_certificate_store'
 require 'nanite/serializer'
 
 module Nanite
-  VERSION = '0.4.1.15.2' unless defined?(Nanite::VERSION)
+  VERSION = '0.4.1.15.3' unless defined?(Nanite::VERSION)
 
   class MapperNotRunning < StandardError; end
 
