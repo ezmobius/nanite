3b7bd71a16fe736c1c25aa9842e6e30a5eb2bde0
Mathias Meyer <meyer@paperplanes.de>
2010-06-14 15:30:46 +0200

    Bumb development version.

--- lib/nanite.rb
+++ lib/nanite.rb
@@ -41,7 +41,7 @@ require 'nanite/security/static_certificate_store'
 require 'nanite/serializer'
 
 module Nanite
-  VERSION = '0.4.1.15.6' unless defined?(Nanite::VERSION)
+  VERSION = '0.4.1.15.7' unless defined?(Nanite::VERSION)
 
   class MapperNotRunning < StandardError; end
 
